ef09c48ef55eaeaa71264377760c5f7df7edc536
User <User@ChristopherC>
2024-11-01 16:29:35 +0700

    Second Commit

--- BADFinal/src/main/Main.java
+++ BADFinal/src/main/Main.java
@@ -219,7 +219,7 @@ public class Main extends Application{
 		arrangeComponentLogin();
 		
 		// Start Scene
-		stage.setTitle("On Boarding Pagesss");
+		stage.setTitle("On Boarding Page");
 		stage.setResizable(false);
 		stage.setScene(sceneOnBoarding);
 		stage.show();
